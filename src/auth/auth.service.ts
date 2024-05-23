d11e9793e9f6ab23fb8dcda4e4ed4befdba87b8d
Carlos Ramirez <carlos@extrasistemas.com>
2024-05-22 23:28:29 -0600

    feat: add method login without jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import * as bycrypt from 'bcrypt';
 import { PrismaClient } from '@prisma/client';
-import { RegisterUserDto } from './dto';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -39,6 +39,42 @@ export class AuthService extends PrismaClient implements OnModuleInit {
           id: newUser.id,
           email: newUser.email,
           name: newUser.name,
+        },
+        token: 'xx1',
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+  async loginUser(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+    try {
+      const user = await this.user.findUnique({
+        where: {
+          email,
+        },
+      });
+      if (!user) {
+        throw new RpcException({
+          status: 409,
+          message: 'not valid user or password',
+        });
+      }
+      const isPasswordValid = bycrypt.compareSync(password, user.password);
+      if (!isPasswordValid) {
+        throw new RpcException({
+          status: 409,
+          message: 'Valid your user or password',
+        });
+      }
+      return {
+        user: {
+          id: user.id,
+          email: user.email,
+          name: user.name,
         },
         token: 'xx1',
       };
